142a36fe8eafb1e8825df84ba2230e84850b3635
Oliver Saxon <olisaxon@Olivers-Mac-mini.local>
2021-03-17 22:25:16 +0000

    FEAT: Script now sets correct hour labels

--- assets/script.js
+++ assets/script.js
@@ -5,22 +5,27 @@ let now = moment().format("ddd, MMM Do h:mm a")
 
 
 
-let startHour = 9
-let endHour = 17;
+let startHour = 8
+let endHour = 16;
 // console.log(hoursInDay)
 // console.log(dayStart.format("h"))
 // console.log(dayEnd.format("h"))
 
 function setTimes() {
 
-    let dayStart = moment().hour(startHour);
-    let dayEnd = moment().hour(endHour)
+    let dayStart = moment().hour(startHour).startOf('hour');
+    let dayEnd = moment().hour(endHour).startOf('hour');
     let hoursInDay = dayEnd.diff(dayStart, 'hours');
-    console.log(dayStart.format('h'));
+    console.log(dayStart.format('H'));
     for(i = 0; i < hoursInDay; i++){
-        dayStart.add(i,'h');
-        timeBlocksEl.children().eq(i).children().eq(0).text(dayStart.format('h'));
-        // console.log(dayStart.add(i,'h').format('h'));
+        if(!i > 0){
+            let hour = dayStart;
+        } else {
+            let hour = dayStart.add(1,'h')
+        };
+        timeBlocksEl.children().eq(i).children().eq(0).text(hour.format('h a'));
+        timeBlocksEl.children().eq(i).children().eq(0).attr('data-hour',hour.format('H'));;
+        console.log(timeBlocksEl.children().eq(i).children().eq(0).attr('data-hour'));
     }
 }
 
