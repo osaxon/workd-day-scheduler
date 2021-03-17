720047e063461e1e5056b3198b186503b15cf71e
Oli <oliverrsaxon@gmail.com>
2021-03-17 23:27:16 +0000

    FEAT: Class changes when hour has past

--- assets/script.js
+++ assets/script.js
@@ -5,41 +5,46 @@ let now = moment().format("ddd, MMM Do h:mm a")
 
 
 
-let startHour = 8
-let endHour = 16;
+let startHour = 9
+let endHour = 17;
 // console.log(hoursInDay)
 // console.log(dayStart.format("h"))
 // console.log(dayEnd.format("h"))
 
 function setTimes() {
-
     let dayStart = moment().hour(startHour).startOf('hour');
-    let dayEnd = moment().hour(endHour).startOf('hour');
-    let hoursInDay = dayEnd.diff(dayStart, 'hours');
-    console.log(dayStart.format('H'));
-    for(i = 0; i < hoursInDay; i++){
-        if(!i > 0){
-            let hour = dayStart;
-        } else {
-            let hour = dayStart.add(1,'h')
-        };
-        timeBlocksEl.children().eq(i).children().eq(0).text(hour.format('h a'));
-        timeBlocksEl.children().eq(i).children().eq(0).attr('data-hour',hour.format('H'));;
-        console.log(timeBlocksEl.children().eq(i).children().eq(0).attr('data-hour'));
-    }
+    let hour = dayStart;
+    $(".time-block").each(function () {
+        $(this).children().eq(0).text(hour.format('h a'))
+        $(this).attr("data-hour",hour.format('H'))
+        hour.add(1,'h');
+    })
+    checkTimes();
+}
+
+function checkTimes() {
+    $(".time-block").each(function () {
+        if($(this).attr("data-hour") < moment().format("H")){
+            console.log($(this).attr("data-hour"))
+            console.log(now)
+            $(this).children().eq(1).addClass("past");
+        } else if ($(this).attr("data-hour") > now) {
+            console.log("future")
+        }
+    })
 }
 
 function timer(){
     currentTimeEl.text(now)
+    console.log(now)
     let timer = setInterval(function(){
         currentTimeEl.text(moment().format("ddd, MMM Do h:mm a"))
         now = moment().format("ddd, MMM Do h:mm a");
-    },60 * 1000);
+        console.log(now)
+    },1500);
     
 }
 
-// how many time blocks
-const timeLabels = ["9am","10am","11am","12am","1pm","2p,","3pm","4pm","5pm"];
 
 timer();
 setTimes()
